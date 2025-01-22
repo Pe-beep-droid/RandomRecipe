9f02ea742d1e63e294b3de6e380c7c555b293d98
Pedro Teixeira <131815011+Pe-beep-droid@users.noreply.github.com>
2025-01-22 11:58:08 +0000

    Update fetchRecipe.mjs

--- netlify/functions/fetchRecipe.mjs
+++ netlify/functions/fetchRecipe.mjs
@@ -1,13 +1,30 @@
-// netlify/functions/fetchRecipe.js
 const fetch = require('node-fetch'); // Netlify supports fetch for serverless functions
 
 exports.handler = async (event, context) => {
   const apiKey = process.env.API_KEY; // Get API Key from environment variables set in Netlify
+  if (!apiKey) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'API Key not configured' })
+    };
+  }
+
   const apiUrl = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}`;
 
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(apiUrl, {
+      headers: { 'Cache-Control': 'no-cache' }
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error fetching recipe: ${response.statusText}`);
+    }
+
     const data = await response.json();
+    if (!data.recipes || data.recipes.length === 0) {
+      throw new Error('No recipes found');
+    }
+
     const recipe = data.recipes[0];
 
     return {
@@ -17,8 +34,7 @@ exports.handler = async (event, context) => {
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Unable to fetch recipe' })
+      body: JSON.stringify({ error: error.message })
     };
   }
 };
-
